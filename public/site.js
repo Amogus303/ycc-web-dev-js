8057c24a511c2806914e558a31893f6191f38ec5
Kilo <you@example.com>
2023-01-25 18:54:39 +0300

    menu13

--- public/site.js
+++ public/site.js
@@ -1,16 +1,28 @@
 let myBtn = document.getElementById('add-btn')
 myBtn.addEventListener('click', addItem)
 
+let countTask = 0 
+
 function addItem(data) {
   let myInput = document.getElementById('do-it')
   let myList = document.getElementById('todo-list')
 
   if (myInput.value !== '') {
+    countTask = countTask +1;
     let newItem = document.createElement('li')
-    newItem.innerText = myInput.value
+    let newBlock = 
+    `<input onclick="finishTask(${countTask})" class="form-check-input me-1"  type="checkbox" value="" id="ch-${countTask}">
+    <label id="${countTask}" class="form-check-label stretched-link" for="ch-${countTask}">${myInput.value}</label>`
+    newItem.innerHTML = newBlock
+    newItem.classList.add('list-group-item')
     myList.appendChild(newItem)
     myInput.value = '';
   }
+}
+
+function finishTask(lableId) {
+let elem = document.getElementById(lableId)
+elem.classList.add('item-completed')
 }
 
 let allbrow = document.addEventListener('keydown', function (event) {
