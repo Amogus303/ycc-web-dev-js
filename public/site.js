2c9d501ba97a6c3ecc80f5e2324c5e2b7d5d3edb
Kilo <you@example.com>
2023-02-01 18:30:00 +0300

    jjjk

--- public/site.js
+++ public/site.js
@@ -12,7 +12,11 @@ function addItem(data) {
     let newItem = document.createElement('li')
     let newBlock = 
     `<input onclick="finishTask(${countTask})" class="form-check-input me-1"  type="checkbox" value="" id="ch-${countTask}">
-    <label id="${countTask}" class="form-check-label stretched-link" for="ch-${countTask}">${myInput.value}</label>`
+    <label id="${countTask}" class="form-check-label stretched-link" for="ch-${countTask}">${myInput.value}</label>
+    <div class="btn-group" role="group" aria-label="Basic outlined example">
+  <button type="button" class="btn btn-outline-primary btn-sm">Удалить</button>
+  <button type="button" class="btn btn-outline-primary btn-sm">Изменить</button>
+</div>`
     newItem.innerHTML = newBlock
     newItem.classList.add('list-group-item')
     myList.appendChild(newItem)
@@ -22,11 +26,20 @@ function addItem(data) {
 
 function finishTask(lableId) {
 let elem = document.getElementById(lableId)
-elem.classList.add('item-completed')
-}
+if(elem.classList.contains ('item-completed')){
+
+elem.classList.remove ('item-completed')} else {
+  elem.classList.add ('item-completed')
+}}
 
 let allbrow = document.addEventListener('keydown', function (event) {
   if (event.keyCode === 13) {
     addItem()
   }
 })
+
+//var list = document.getElementById('todo list')
+//text.innerHTML = ""
+
+//var button = document.getElementById('btn-del')
+//button.parentElement.parentElement.parentNode.removeChild(button.parentElement.parentElement)
